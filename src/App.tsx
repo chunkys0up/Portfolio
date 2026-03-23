4bbcb78044954c59cbe6e4676d3db200a8c8062d
Patheit <andrewtnguyen0106@gmail.com>
2026-03-23 00:11:28 -0700

    updated to hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter, Routes, Route, Link } from 'react-router-dom';
 import Image_gallery from './pages/ImageGallery';
 import AboutMe from './pages/AboutMe';
 import './styles.css';
 
 function App() {
-
     return (
-        <BrowserRouter>
+        <HashRouter>
             <nav>
                 <Link to="/">About</Link>
                 <Link to="/gallery">Gallery</Link>
             </nav>
-
             <Routes>
                 <Route path="/" element={<AboutMe />} />
                 <Route path="/gallery" element={<Image_gallery />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
